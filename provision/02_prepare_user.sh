146a6eb8c0b2155d3c66225fe6d189756b9fafec
Julien Pardo <PardoJ@users.noreply.github.com>
2017-06-28 09:16:54 +0200

    Update 02_prepare_user.sh

--- provision/02_prepare_user.sh
+++ provision/02_prepare_user.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 
 ########################################################################################################################
-# Configure l'accès au web passant par le proxy EDF. NNI et password SESAME à utiliser.
-echo '====== PROXY'
-# Le script du proxy
-echo '
-#!/bin/sh -x
-
-export login=${LOGIN}
-export password=${PASSWORD}
-export url=pcyvipncp2n.edf.fr
-
-unset http_proxy
-unset https_proxy   
-
-curl --max-time 5 -k https://$login:$password@authpcy.edf.fr/?cfru=aHR0cDovL3d3dy5nb29nbGUuZnIv
-
-export http_proxy=http://$login:$password@$url:3128
-export https_proxy=$http_proxy
-export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"
-' > ~/proxy.sh
-
-########################################################################################################################
 # Configure un virtualenv avec pip.
 echo '====== VIRTUALENV'
 mkdir -p ~/.ICE
