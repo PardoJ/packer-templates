473da68137d5eec3592e4855b965f0d976a5e7db
Julien Pardo <PardoJ@users.noreply.github.com>
2017-06-28 09:16:32 +0200

    Add files via upload

---
+++ provision/01_prepare_vm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+########################################################################################################################
+# Banner de connection
+echo "#############################################################################
+#                         Console ansible                                   #
+#        Debian 9.0-x86_32; Python 2.7; Virtualenv; Ansible 2.3         #
+#                                                                           #
+#           https://si-devops-gitlab.edf.fr/ice-ice/_SYSTEM_TEAM_           #
+#   https://si-devops-confluence.edf.fr/display/ICEICE/Espace+SYSTEM+TEAM   #
+#############################################################################" > /etc/motd
